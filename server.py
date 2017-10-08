4c2776d13ebb271bbd6418b4b9146f63107e99e1
Jimmy <chen.jimmym@gmail.com>
2017-10-08 14:21:08 -0700

    added part two of form validation

--- server.py
+++ server.py
@@ -12,12 +12,16 @@ def showSubmitted():
     location = request.form['location']
     language = request.form['language']
     comment = request.form['comment']
-    if len(name) > 0 and len(comment) > 0:
+    if len(name) > 0 and len(name) < 121 and len(comment) > 0 and len(comment) < 121:
         return render_template('result.html', name = name, location = location, language = language, comment = comment)
     if len(name) < 1:
         flash("Name cannot be empty!")
+    elif len(name) > 120:
+        flash("Name cannot be long than 120 characters.")
     if len(comment) < 1:
         flash("Comment cannot be empty!")
+    elif len(comment) > 120:
+        flash("Comment cannot be long than 120 characters.")
     return redirect('/')
 
 app.run(debug=True)
